57586a1739ae169f5ac60736159c259de147e8e4
Chris Kocks <cgkocks@gmail.com>
2019-12-28 21:48:05 -0500

    Update FacePPemotion.js

--- FacePPemotion.js
+++ FacePPemotion.js
@@ -1,6 +1,6 @@
 // Demonstrate POST a photo to Face++ 
-var facePlusPlusKey = "EOR0Z1YLaiJvA-bkcOlXOILEzOpCiYoF"
-var facePlusPlusSecret = "8lR-X_CeSCjQf-grdECHk0Se85xILJYE"    
+var facePlusPlusKey = "<YourKey>"
+var facePlusPlusSecret = "<YourSecret"    
 var base64String 
 misty.TakePicture("MyPic", 200, 200, true, true)
 
